8e034ddea1ebe89257ea8ff17d9368d37d3e0949
unknown <salinasleonardo@live.com>
2020-05-13 21:00:19 -0300

    Alteraçoes do projeto CLEAR

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <locale.h>
+#include <unistd.h>
+
+#if defined(__MINGW32__) || defined(_MSC_VER)
+#define limpar_input() fflush(stdin)
+#define clear() system("cls")
+#else
+#define limpar_input() __fpurge(stdin)
+#define clear() system("clear")
+#endif
 
 int main() {
     setlocale(LC_ALL, "");
 
+	printf("Salveeeeee");
+	getchar();
+	clear();
+	
+
+	
 }
