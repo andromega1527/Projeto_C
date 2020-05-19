353c3e27f9a1492fbb371ecb740a3d42b1777f62
Leonam <andrejc2008@hotmail.com>
2020-05-19 18:56:07 -0300

    questions system working

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <locale.h>
 #include <unistd.h>
+#include <string.h>
 
 #if defined(__MINGW32__) || defined(_MSC_VER)
 #define limpar_input() fflush(stdin)
@@ -11,25 +12,65 @@
 #define clear() system("clear")
 #endif
 
-void get_questions(char questions[]) {
+bool is_digit_first_position(char string[]) {
+	char digit[] = {string[0]};
+	if(strcmp(digit, "1") == 0 || strcmp(digit, "2") == 0 || strcmp(digit, "0") == 0)
+		return true;
+	return false;
+}
+
+void get_questions(char questions[][100]) {
 	FILE *file = fopen("questions.txt", "rt");
+	int count=0;
 	char line[119];
-	char *result = fgets(line, 119, file);
+	char *result;
+	for(int i=0; i<100; i++) {
+		result = fgets(line, 100, file);
+		if(is_digit_first_position(result)) {
+			strcpy(questions[count], result);
+			count++;
+		}
+	}
+	fclose(file);
+}
+
+void get_responses(char responses[][100]) {
+	FILE *file = fopen("questions.txt", "rt");
+	int count=0;
+	char line[119];
+	char *result;
+	for(int i=0; i<100; i++) {
+		result = fgets(line, 100, file);
+		if(!is_digit_first_position(result)) {
+			strcpy(responses[count], result);
+			count++;
+		}
+	}
+	fclose(file);
 }
 
 int main() {
     setlocale(LC_ALL, "");
-	char responses[20], questions[20][4];
+	char responses[80][100], questions[20][100], responses_users[20][2];
+	int count_responses=0;
 
 	printf("Salveeeeee");
 	getchar();
 	clear();
-	printf("Bem vindo ao jogo de perguntas e respostas!\nAqui voce ter� que Responder a perguntas sobre o tema que voc� escolher");
+	printf("Bem vindo ao jogo de perguntas e respostas!\nAqui voce terá que Responder a perguntas sobre o tema que você escolher\n");
+
+	get_questions(questions);
+	get_responses(responses);
 
 	for(int i=0; i<20; i++) {
 		printf("%s", questions[i]);
-		for(int j=0; j<4; j++)
-			printf("%s", questions[i][j]);
-		scanf("%s", &responses);
+		for(int j=0; j<4; j++) {
+			printf("\n%s", responses[count_responses]);
+			count_responses++;
+		}
+
+		printf("\n\n");
+		scanf("%s", responses_users[i]);
+		clear();
 	}
 }
